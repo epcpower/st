e19b6b1cd370334ee9a30c94b87739ab3c2bdb3a
Kyle Altendorf <kyle.altendorf@epcpower.com>
2015-12-13 22:10:02 -0500

    Fix setup.py and epyq.py -> __main__.py
    Ref T121

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="EPyQ",
     version="0.1",
     packages=find_packages(),
-    entry_points={'gui_scripts': ['epyq = epyq.epyq:main']}
+    entry_points={'gui_scripts': ['epyq = epyq.__main__:main']}
 )
