f3c65324bb0953acbce5e8856800e76e157994bd
Kyle Altendorf <kyle.altendorf@epcpower.com>
2015-12-19 20:40:11 -0500

    Add profile_yappi.py for profiling using yappi
    Note that while yappi supports multiple threads, it also seems to
    have significant (100 percent?) overhead in this case.
    
    Ref T129

---
+++ tests/profile_yappi.py
@@ -0,0 +1,38 @@
+# http://stackoverflow.com/a/32458998/228539
+"""Replace python thread identifier by TID."""
+# Imports
+import threading, ctypes
+# Define get tid function
+def gettid():
+    """Get TID as displayed by htop."""
+    libc = 'libc.so.6'
+    for cmd in (186, 224, 178):
+        tid = ctypes.CDLL(libc).syscall(cmd)
+        if tid != -1:
+            return tid
+
+
+import yappi
+import epyq.__main__
+
+
+class Bunch:
+    # http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named/?in=user-97991
+
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+
+def main():
+    print('Main TID: {}'.format(gettid()))
+    args = Bunch(can='/epc/AFE_CAN_ID247_FACTORY.sym', generate=False)
+    yappi.start()
+    exit_value = epyq.__main__.main(args=args)
+    yappi.stop()
+    yappi.get_func_stats().save('yappi.stats', type='pstat')
+    yappi.get_thread_stats().print_all()
+    return exit_value
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
