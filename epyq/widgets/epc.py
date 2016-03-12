6a763ca697eabde5541540a83b94e6e884926ad9
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-03-12 15:33:23 -0500

    Make Epc a Tx widget
    Ref T148

--- epyq/widgets/epc.py
+++ epyq/widgets/epc.py
@@ -2,26 +2,33 @@
 
 #TODO: """DocString if there is one"""
 
-import epyq.widgets.abstractwidget
+import epyq.widgets.abstracttxwidget
 import os
 from PyQt5.QtCore import (pyqtSignal, pyqtProperty,
-                          QFile, QFileInfo, QTextStream)
+                          QFile, QFileInfo, QTextStream, Qt)
 
 # See file COPYING in this source tree
 __copyright__ = 'Copyright 2016, EPC Power Corp.'
 __license__ = 'GPLv2+'
 
 
-class Epc(epyq.widgets.abstractwidget.AbstractWidget):
+class Epc(epyq.widgets.abstracttxwidget.AbstractTxWidget):
     def __init__(self, parent=None):
         ui_file = os.path.join(QFileInfo.absolutePath(QFileInfo(__file__)),
                                'epc.ui')
 
-        epyq.widgets.abstractwidget.AbstractWidget.__init__(self,
+        epyq.widgets.abstracttxwidget.AbstractTxWidget.__init__(self,
                 ui=ui_file, parent=parent)
+
+        # TODO: CAMPid 398956661298765098124690765
+        self.ui.value.editingFinished.connect(self.widget_value_changed)
 
         self._frame = None
         self._signal = None
+
+    def widget_value_changed(self):
+        epyq.widgets.abstracttxwidget.AbstractTxWidget.widget_value_changed(
+            self, self.ui.value.text())
 
     def set_value(self, value):
         if self.signal_object is not None:
@@ -30,12 +37,23 @@ class Epc(epyq.widgets.abstractwidget.AbstractWidget):
             else:
                 value = self.signal_object.format_float()
         elif value is None:
-            value = '-'
+            # TODO: quit hardcoding this and it's better implemented elsewhere
+            value = '{0:.2f}'.format(0)
         else:
             # TODO: quit hardcoding this and it's better implemented elsewhere
             value = '{0:.2f}'.format(value)
 
         self.ui.value.setText(value)
+
+    @epyq.widgets.abstracttxwidget.AbstractTxWidget.tx.setter
+    def tx(self, tx):
+        epyq.widgets.abstracttxwidget.AbstractTxWidget.tx.fset(self, tx)
+
+        self.ui.value.acceptsDrops = self.tx
+
+        self.ui.value.setMouseTracking(self.tx)
+        self.ui.value.setReadOnly(not self.tx)
+        self.ui.value.setFocusPolicy(Qt.StrongFocus if self.tx else Qt.NoFocus)
 
 
 if __name__ == '__main__':
