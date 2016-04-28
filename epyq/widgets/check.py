39410f27f247ab110e81df13e4fcc1db2821fe8f
tsnider <todd.snider@epcpower.com>
2016-04-28 09:15:02 -0400

    Add checkbox widget.

---
+++ epyq/widgets/check.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#TODO: """DocString if there is one"""
+
+import epyq.widgets.abstracttxwidget
+import os
+from PyQt5.QtCore import (pyqtSignal, pyqtProperty,
+                          QFile, QFileInfo, QTextStream, Qt, QEvent,
+                          QTimer)
+from PyQt5.QtGui import QMouseEvent
+
+# See file COPYING in this source tree
+__copyright__ = 'Copyright 2016, EPC Power Corp.'
+__license__ = 'GPLv2+'
+
+
+class Check(epyq.widgets.abstracttxwidget.AbstractTxWidget):
+    def __init__(self, parent=None):
+        ui_file = os.path.join(QFileInfo.absolutePath(QFileInfo(__file__)),
+                               'check.ui')
+
+        epyq.widgets.abstracttxwidget.AbstractTxWidget.__init__(self,
+                ui=ui_file, parent=parent)
+
+        # TODO: CAMPid 398956661298765098124690765
+        self.ui.value.toggled.connect(self.widget_value_changed)
+
+        self._frame = None
+        self._signal = None
+
+    def set_value(self, value):
+        # TODO: quit hardcoding this and it's better implemented elsewhere
+        if self.signal_object is not None:
+            value = bool(self.signal_object.value)
+        elif value is None:
+            value = False
+        else:
+            value = bool(value)
+
+        self.ui.value.setChecked(value)
+
+    def signal_value_changed(self, value):
+        self.ui.value.setChecked(bool(value))
+
+
+if __name__ == '__main__':
+    import sys
+
+    print('No script functionality here')
+    sys.exit(1)     # non-zero is a failure
