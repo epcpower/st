a43f2a799c5a5606c88910b3083eb14a7d4dd6f9
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-05-21 17:20:12 -0400

    Make generaterevision.py work in Linux too

--- epyq/generaterevision.py
+++ epyq/generaterevision.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
 
-hash = subprocess.check_output([os.path.join('c:/', 'Program Files', 'Git', 'bin', 'git.exe'), 'rev-parse', 'HEAD'])
+try:
+    hash = subprocess.check_output([os.path.join('c:/', 'Program Files', 'Git', 'bin', 'git.exe'), 'rev-parse', 'HEAD'])
+except FileNotFoundError:
+    hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
 
 out_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'revision.py')
 
