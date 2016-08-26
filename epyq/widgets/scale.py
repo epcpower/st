6d807604a5bf1853876b15ae22ce81ba952bce85
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-08-26 17:47:24 -0400

    Add color range control to scale widget
    Ref T324

--- epyq/widgets/scale.py
+++ epyq/widgets/scale.py
@@ -5,7 +5,8 @@
 import epyq.mixins
 import epyq.widgets.abstractwidget
 import os
-from PyQt5.QtCore import pyqtSignal, pyqtProperty, QFileInfo
+from PyQt5.QtCore import Qt, pyqtSignal, pyqtProperty, QFileInfo
+from PyQt5.QtGui import QColor
 
 # See file COPYING in this source tree
 __copyright__ = 'Copyright 2016, EPC Power Corp.'
@@ -23,8 +24,99 @@ class Scale(epyq.widgets.abstractwidget.AbstractWidget,
 
         epyq.mixins.OverrideRange.__init__(self)
 
+        self._breakpoints = [self._min + (self._max - self._min) * n
+                             for n in [0.10, 0.25, 0.75, 0.90]]
+        self._colors = [Qt.darkRed,
+                        Qt.darkYellow,
+                        Qt.darkGreen,
+                        Qt.darkYellow,
+                        Qt.darkRed]
+
         self._frame = None
         self._signal = None
+
+        self.update_configuration()
+
+    @pyqtProperty(float)
+    def lower_red_breakpoint(self):
+        return self._breakpoints[0]
+
+    @lower_red_breakpoint.setter
+    def lower_red_breakpoint(self, breakpoint):
+        self._breakpoints[0] = breakpoint
+        self.update_configuration()
+
+    @pyqtProperty(float)
+    def lower_yellow_breakpoint(self):
+        return self._breakpoints[1]
+
+    @lower_yellow_breakpoint.setter
+    def lower_yellow_breakpoint(self, breakpoint):
+        self._breakpoints[1] = breakpoint
+        self.update_configuration()
+
+    @pyqtProperty(float)
+    def upper_yellow_breakpoint(self):
+        return self._breakpoints[2]
+
+    @upper_yellow_breakpoint.setter
+    def upper_yellow_breakpoint(self, breakpoint):
+        self._breakpoints[2] = breakpoint
+        self.update_configuration()
+
+    @pyqtProperty(float)
+    def upper_red_breakpoint(self):
+        return self._breakpoints[3]
+
+    @upper_red_breakpoint.setter
+    def upper_red_breakpoint(self, breakpoint):
+        self._breakpoints[3] = breakpoint
+        self.update_configuration()
+
+    @pyqtProperty(QColor)
+    def lower_red_color(self):
+        return self._colors[0]
+
+    @lower_red_color.setter
+    def lower_red_color(self, color):
+        self._colors[0] = color
+        self.update_configuration()
+
+    @pyqtProperty(QColor)
+    def lower_yellow_color(self):
+        return self._colors[1]
+
+    @lower_yellow_color.setter
+    def lower_yellow_color(self, color):
+        self._colors[1] = color
+        self.update_configuration()
+
+    @pyqtProperty(QColor)
+    def green_color(self):
+        return self._colors[2]
+
+    @green_color.setter
+    def green_color(self, color):
+        self._colors[2] = color
+        self.update_configuration()
+
+    @pyqtProperty(QColor)
+    def upper_yellow_color(self):
+        return self._colors[3]
+
+    @upper_yellow_color.setter
+    def upper_yellow_color(self, color):
+        self._colors[3] = color
+        self.update_configuration()
+
+    @pyqtProperty(QColor)
+    def upper_red_color(self):
+        return self._colors[4]
+
+    @upper_red_color.setter
+    def upper_red_color(self, color):
+        self._colors[4] = color
+        self.update_configuration()
 
     def set_value(self, value):
         self.ui.scale.setValue(value)
@@ -38,6 +130,11 @@ class Scale(epyq.widgets.abstractwidget.AbstractWidget,
 
     def set_unit_text(self, units):
         self.ui.units.setText('[{}]'.format(units))
+
+    def update_configuration(self):
+        self.ui.scale.setColorRanges(self._colors, self._breakpoints)
+
+        self.repaint()
 
 
 if __name__ == '__main__':
