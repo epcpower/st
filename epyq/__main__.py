0e4750fb6d1c3b7d1392deddb5e48421e5f1f48f
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-09-21 06:24:41 -0700

    Add excepthook to write to stderr

--- epyq/__main__.py
+++ epyq/__main__.py
@@ -106,10 +106,74 @@ class StackedHistory:
     def focus_previous(self, steps=1):
         self.stacked_widget.setCurrentIndex(self.history[-(steps+1)])
 
+
+embedded = os.environ.get('QT_QPA_PLATFORM') == 'linuxfb'
+
+
+ # TODO: Consider updating from...
+#       http://die-offenbachs.homelinux.org:48888/hg/eric/file/a1e53a9ffcf3/eric6.py#l134
+
+# TODO: deal with licensing for swiped code (GPL3)
+#       http://die-offenbachs.homelinux.org:48888/hg/eric/file/a1e53a9ffcf3/LICENSE.GPL3
+
+def excepthook(excType, excValue, tracebackobj):
+    """
+    Global function to catch unhandled exceptions.
+
+    @param excType exception type
+    @param excValue exception value
+    @param tracebackobj traceback object
+    """
+    separator = '-' * 70
+    email = "kyle.altendorf@epcpower.com"
+
+    try:
+        hash = 'Revision Hash: {}\n\n'.format(epyq.revision.hash)
+    except:
+        hash = ''
+
+    notice = \
+        """An unhandled exception occurred. Please report the problem via email to:\n"""\
+        """\t\t{email}\n\n{hash}"""\
+        """A log has been written to "{log}".\n\nError information:\n""".format(
+        email=email, hash=hash, log=log.name)
+    # TODO: add something for version
+    versionInfo=""
+    timeString = time.strftime("%Y-%m-%d, %H:%M:%S")
+
+    tbinfofile = io.StringIO()
+    traceback.print_tb(tracebackobj, None, tbinfofile)
+    tbinfofile.seek(0)
+    tbinfo = tbinfofile.read()
+    errmsg = '%s: \n%s' % (str(excType), str(excValue))
+    sections = [separator, timeString, separator, errmsg, separator, tbinfo]
+    msg = '\n'.join(sections)
+
+    complete = str(notice) + str(msg) + str(versionInfo)
+
+    sys.stderr.write(complete)
+
+    if not embedded:
+        errorbox = QMessageBox()
+        errorbox.setWindowTitle("EPyQ")
+        errorbox.setIcon(QMessageBox.Critical)
+
+        # TODO: CAMPid 980567566238416124867857834291346779
+        ico_file = os.path.join(QFileInfo.absolutePath(QFileInfo(__file__)), 'icon.ico')
+        ico = QtGui.QIcon(ico_file)
+        errorbox.setWindowIcon(ico)
+
+        errorbox.setText(complete)
+        errorbox.exec_()
+
+
 def main(args=None):
     print('starting epyq')
 
     app = QApplication(sys.argv)
+    sys.excepthook = excepthook
+    app.setStyleSheet('QMessageBox {{ messagebox-text-interaction-flags: {}; }}'
+                      .format(Qt.TextBrowserInteraction))
     app.setOrganizationName('EPC Power Corp.')
     app.setApplicationName('EPyQ HMI')
 
