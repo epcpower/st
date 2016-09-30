84a686ee30d18928768732a6091400d42aeb1096
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-09-30 15:45:23 -0400

    With demo trace works with no rx_interval, so drop it to 0.2
    Just in case, leave a bit of protection in there.

--- epyq/__main__.py
+++ epyq/__main__.py
@@ -283,7 +283,7 @@ def main(args=None):
                                 tabs=[],
                                 elements=[epyq.device.Elements.dash,
                                           epyq.device.Elements.nv],
-                                rx_interval=0.5,
+                                rx_interval=0.2,
                                 edit_actions=edit_actions)
 
     # TODO: CAMPid 9757656124812312388543272342377
