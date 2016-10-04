bdb7111f93a07983f5399f27cfeaadd955d0ffc4
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-10-04 13:34:44 -0400

    Rename to epyqlib

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="EPyQ",
+    name="EPyQ Library",
     version="0.1",
     author="EPC Power Corp.",
     classifiers=[
         ("License :: OSI Approved :: "
          "GNU General Public License v2 or later (GPLv2+)")
     ],
-    packages=find_packages(),
-    entry_points={'gui_scripts': ['epyq = epyq.__main__:main']}
+    packages=find_packages()
 )
