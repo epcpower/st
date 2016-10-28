538e56465d91f9c8a9e25d14c2399d6b556c8b17
Kyle Altendorf <kyle.altendorf@epcpower.com>
2016-10-28 13:16:33 -0700

    Update venv.py to use tox

--- venv.py
+++ venv.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-from collections import OrderedDict
+import errno
 import os
 import platform
 import shutil
-import subprocess
+import stat
 import sys
+
+if sys.platform not in ['win32', 'linux']:
+    raise Exception("Unsupported sys.platform: {}".format(sys.platform))
 
 # TODO: CAMPid 097541134161236179854863478319
 try:
@@ -23,26 +26,9 @@ except ImportError:
 
     sys.exit(1)
 
-# TODO: redo as a bootstrap script
-#       https://virtualenv.readthedocs.org/en/latest/reference.html#extending-virtualenv
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--bin')
-parser.add_argument('--activate')
-parser.add_argument('--no-ssl-verify', action='store_true')
-parser.add_argument('--virtualenv', '--venv', default=os.path.join(os.getcwd(), 'venv'))
-parser.add_argument('--in-virtual', action='store_true', default=False)
-parser.add_argument('--rebuild', action='store_true')
-parser.add_argument('--no-designer', action='store_true')
-
-args = parser.parse_args()
-
-# TODO: let this be the actual working directory
-myfile = os.path.realpath(__file__)
-mydir = os.path.dirname(myfile)
 
 # TODO: CAMPid 9811163648546549994313612126896
-def pip_install(package, no_ssl_verify, virtual=False, editable=False):
+def pip_install(package, no_ssl_verify=False, virtual=False, editable=False):
     pip_parameters = ['install']
     if no_ssl_verify:
         pip_parameters.append('--index-url=http://pypi.python.org/simple/')
@@ -56,199 +42,68 @@ def pip_install(package, no_ssl_verify, virtual=False, editable=False):
     if pip.main(pip_parameters):
         raise Exception('Failed to install {}'.format(package))
 
-if not args.in_virtual:
-    if args.rebuild:
-        shutil.rmtree(args.virtualenv, ignore_errors=True)
 
+# http://stackoverflow.com/a/10840586/228539
+def silentremove(filename):
     try:
-        os.mkdir(args.virtualenv)
-    except FileExistsError:
-        print('')
-        print('')
-        print('    Directory already exists and must be deleted to create the virtual environment')
-        print('')
-        print('        {args.virtualenv}'.format(**locals()))
-        print('')
+        os.remove(filename)
+    except OSError as e:  # this would be "except OSError, e:" before Python 2.6
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise # re-raise exception if a different error occured
 
-        sys.exit(1)
 
-    if sys.platform not in ['win32', 'linux']:
-        raise Exception("Unsupported sys.platform: {}".format(sys.platform))
-
-    if sys.platform == 'win32':
-        bin = os.path.join(args.virtualenv, 'Scripts')
+# TODO: CAMPid 0238493420143087667542054268097120437916848
+# http://stackoverflow.com/a/21263493/228539
+def del_rw(action, name, exc):
+    os.chmod(name, stat.S_IWRITE)
+    if os.path.isdir(name):
+        os.rmdir(name)
     else:
-        bin = os.path.join(args.virtualenv, 'bin')
+        os.remove(name)
 
-    activate = os.path.join(bin, 'activate')
 
-    pip_install('virtualenv', args.no_ssl_verify)
+files = ['activate', 'activate.bat', 'designer', 'designer.bat', 'designer.vbs']
 
-    virtualenv_command = [sys.executable, '-m', 'virtualenv', '--system-site-packages', args.virtualenv]
-    returncode = subprocess.call(virtualenv_command)
+for file in files:
+    silentremove(file)
 
-    if returncode != 0:
-        raise Exception("Received return code {} when running {}"
-                        .format(result.returncode, virtualenv_command))
+try:
+    shutil.rmtree('venv', onerror=del_rw)
+except FileNotFoundError:
+    pass
 
-    virtualenv_python = os.path.realpath(os.path.join(bin, 'python'))
-    virtualenv_python_command = [virtualenv_python,
-                                 myfile,
-                                 '--bin', bin,
-                                 '--activate', activate,
-                                 '--in-virtual']
-    if args.no_ssl_verify:
-        virtualenv_python_command.append('--no-ssl-verify')
 
-    if args.no_designer:
-        virtualenv_python_command.append('--no-designer')
+pip_install('tox')
+import tox
 
-    returncode = subprocess.call(virtualenv_python_command)
+tox_arguments = [
+    '-e', 'devenv',
+    '--recreate',
+    *sys.argv[1:]
+]
 
-    sys.exit(returncode)
-else:
-    def setup(path):
-        backup = os.getcwd()
-        os.chdir(path)
-        pip_install('.', args.no_ssl_verify, virtual=True, editable=True)
-        os.chdir(backup)
+config = tox.session.prepare(tox_arguments)
+tox.session.Session(config).runcommand()
 
-    src = os.path.join(os.getcwd(), args.virtualenv, 'src')
-    os.makedirs(src, exist_ok=True)
 
-    packages = []
+if sys.platform == 'win32':
+    import ctypes
+    try:
+        ctypes.windll.LoadLibrary("PCANBasic")
+    except OSError:
+        print('')
+        print('')
+        print('    Unable to load PCANBasic.dll, it is recommended you')
+        print('    install the PEAK drivers: ')
+        print('')
+        print('        http://www.peak-system.com/produktcd/Drivers/PeakOemDrv.exe')
 
-    for package in packages:
-        pip_install(package, args.no_ssl_verify, virtual=True)
 
-    zip_repos = OrderedDict([
-        ('python-can', 'https://bitbucket.org/altendky/python-can/get/'
-                      '076a7864f1e292647a501ae60ea90f62b5703d71.zip'),
-        ('canmatrix', 'https://github.com/ebroecker/canmatrix/archive/'
-                     'c64004b785febe4613677ab0107f1bc63b01e339.zip'),
-        ('bitstruct', 'https://github.com/altendky/bitstruct/archive/'
-                     '129a72e290c533654a91bd556b1d4b0822df423f.zip'),
-        ('fontawesome', 'https://github.com/FortAwesome/Font-Awesome/archive/v4.6.3.zip'),
-        ('metropolis', 'https://github.com/chrismsimpson/Metropolis/archive/16882c2c2cb58405fd6a7d6a932a1dfc573b6813.zip')
-    ])
-
-#     import git
-#
-#     git_repos = OrderedDict([
-#         ('epyqlib', ('https://epc-phab.exana.io/diffusion/ST',
-#                      '9709bcf7bb147c8e05d39cc15f269dba589ebcae'))
-#     ])
-#
-#     for name, (url, hash) in git_repos.items():
-#        dir = os.path.join(src, name)
-#        repo = git.Repo.clone_from(url, dir)
-#        repo.git.checkout(hash)
-#        setup(dir)
-
-    pip_install('git-app-version', args.no_ssl_verify, virtual=True)
-    pip_install('requests', args.no_ssl_verify, virtual=True)
-    import requests
-    import zipfile
-    import io
-    import shutil
-    for name, url in zip_repos.items():
-        try:
-            response = requests.get(url, verify=not args.no_ssl_verify)
-        except requests.exceptions.SSLError:
-            print('')
-            print('        SSL error occurred while requesting:')
-            print('            {}'.format(url))
-            print('')
-            print('        You probably want to use --no-ssl-verify')
-            print('')
-
-            sys.exit(1)
-
-        zip_data = io.BytesIO()
-        zip_data.write(response.content)
-        zip_file = zipfile.ZipFile(zip_data)
-        zip_dir = os.path.split(zip_file.namelist()[0])[0]
-        zip_file.extractall(path=src)
-        try:
-            shutil.rmtree(os.path.join(src, name))
-        except FileNotFoundError:
-            pass
-        shutil.move(os.path.join(src, zip_dir),
-                    os.path.join(src, name))
-        # TODO: remove this because it is a goofy workaround for the issue being discussed
-        #       over in https://github.com/ebroecker/canmatrix/commit/084e1e01eb750adb46e9e33a0d94fadcbf2cc896
-        if name == 'canmatrix':
-            import shutil
-            shutil.copy(os.path.join(os.path.dirname(__file__), 'canmatrix.setup.py'),
-                        os.path.join(args.virtualenv, 'src', 'canmatrix', 'setup.py'))
-        if name not in ['fontawesome', 'metropolis']:
-            setup(os.path.join(src, name))
-
-    setups = []
-    for root, dirs, files in os.walk('sub'):
-        if 'setup.py' in files:
-            setups.append(os.path.join(os.getcwd(), root))
-
-    for path in setups:
-        setup(path)
-
-    # TODO: Figure out why this can't be moved before other installs
-    #       Dependencies maybe?
-    setup(os.getcwd())
-
-    activate = args.activate
-    if sys.platform == 'win32':
-        path_variables = OrderedDict([
-            ('PYQTDESIGNERPATH', [
-                ['epyq'],
-                ['epyq', 'widgets']
-            ]),
-            ('PYTHONPATH', [
-                ['']
-            ])
-        ])
-
-        set_commands = []
-        for name, paths in path_variables.items():
-            command = 'set {}='.format(name)
-            paths = [os.path.join('%cd%', *p) for p in paths]
-            command += ';'.join(paths)
-            set_commands.append(command)
-        
-        with open('activate.bat', 'w') as f:
-            activate = activate.replace('\\', '/')
-            for command in set_commands:
-                f.write(command + '\n')
-            f.write('{}\n'.format(activate))
-
-    with open('activate', 'w', newline='') as f:
-        f.write('export PYQTDESIGNERPATH="{root}/sub/epyqlib/epyqlib:{root}/sub/epyqlib/epyqlib/widgets"\n'
-                .format(root=os.getcwd()))
-        f.write('source {}\n'.format(activate))
-
-    files = ['designer', 'designer.bat', 'designer.vbs']
-    for file in files:
-        shutil.copy(os.path.join(mydir, file), file)
-
-    if sys.platform == 'win32':
-        import ctypes
-        try:
-            ctypes.windll.LoadLibrary("PCANBasic")
-        except OSError:
-            print('')
-            print('')
-            print('    Unable to load PCANBasic.dll, it is recommended you')
-            print('    install the PEAK drivers: ')
-            print('')
-            print('        http://www.peak-system.com/produktcd/Drivers/PeakOemDrv.exe')
-
-    print('')
-    print('')
-    print('    To use the new virtualenv:')
-    print('')
-    print('        posix: source activate')
-    if sys.platform == 'win32':
-        print('        win32: activate')
-    print('')
-
-    sys.exit(0)
+print('')
+print('')
+print('    To use the new virtualenv:')
+print('')
+print('        posix: source activate')
+if sys.platform == 'win32':
+    print('        win32: activate')
+print('')
