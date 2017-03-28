c5a2325a18d22abc8228113a966731973a113157
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-03-28 10:55:05 -0400

    Add epyqlib.utils.general.spy decorator

--- epyqlib/utils/general.py
+++ epyqlib/utils/general.py
@@ -2,6 +2,7 @@ import collections
 import itertools
 import math
 import os
+import threading
 import time
 import zipfile
 
@@ -131,3 +132,45 @@ class append_to_method:
         print('assigned {} to {}.{}'.format(f, cls.__name__, self.name))
 
         return cls
+
+
+def spy(*ignore):
+    """
+    Make sure to ignore callables that are more than just their __call__
+    because this will destroy everything else.
+    """
+    def inner(cls):
+        class Spy(cls):
+            def __getattribute__(self, name):
+                attribute = super().__getattribute__(name)
+                if hasattr(attribute, '__call__') and name not in ignore:
+
+                    def mark(self, marker):
+                        print('{marker} '
+                              '>Thread Id 0x{thread_id:012x}< '
+                              '{repr} '
+                              '{marker} '
+                              '{name}()'.format(
+                                marker=marker,
+                                repr='<{cls} object at 0x{id:012x}>'.format(
+                                    cls=type(self).__bases__[0].__name__,
+                                    id=id(self)
+                                ),
+                                name=name,
+                                thread_id=threading.get_ident()
+                        ))
+
+                    def report(*args, **kwargs):
+                        mark(self, ' +')
+                        result = attribute(*args, **kwargs)
+                        mark(self, '- ')
+
+                        return result
+
+                    return report
+                else:
+                    return attribute
+
+        return Spy
+
+    return inner
