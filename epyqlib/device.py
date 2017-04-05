b8a8a63bba67c756dc839eae6970b014085056f3
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-04-05 08:35:17 -0700

    Also stop device's looping set when terminating
    Ref T505

--- epyqlib/device.py
+++ epyqlib/device.py
@@ -712,6 +712,7 @@ class Device:
 
     def terminate(self):
         self.neo_frames.terminate()
+        self.nv_looping_set.stop()
         logging.debug('{} terminated'.format(object.__repr__(self)))
 
 
