3b4928328011293b3af5eb4a2ee1a097320f37fe
Kyle Altendorf <sda@fstab.net>
2017-06-09 12:28:20 -0400

    Update .ico/.png for hi-dpi work
    Ref T604

---
+++ epyq/svg_to_ico.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import subprocess
+
+
+def print_check_call(command, *args, **kwargs):
+    print('Launching {}'.format(repr(command[0])))
+    print('\n'.join('    {}'.format(repr(c)) for c in command))
+    subprocess.check_call(command, *args, **kwargs)
+
+
+subprocess.check_call([
+    'inkscape',
+    '-z',
+    '-e', 'icon.png',
+    '-w', '256',
+    '-h', '256',
+    'icon.svg',
+])
+
+subprocess.check_call([
+    'convert',
+    'icon.png',
+    '-define', 'icon:auto-resize=256,128,96,64,48,32,24,16',
+    'icon.ico',
+])
+
+for f in ('icon.ico', 'icon.png'):
+    shutil.copy(f, os.path.join('..', 'sub', 'epyqlib', 'epyqlib'))
