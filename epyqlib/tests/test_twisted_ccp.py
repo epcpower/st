ccc0c47370103e09570c33d8724498ebcc93a1a5
Kyle Altendorf <sda@fstab.net>
2017-08-16 16:04:08 -0400

    Catch up CCP test
    Ref T692

--- epyqlib/tests/test_twisted_ccp.py
+++ epyqlib/tests/test_twisted_ccp.py
@@ -6,7 +6,7 @@ import epyqlib.busproxy
 import epyqlib.twisted.busproxy
 import epyqlib.twisted.cancalibrationprotocol as ccp
 import pytest
-import qt5reactor
+import sys
 
 from PyQt5.QtCore import QTimer
 
@@ -49,17 +49,42 @@ from PyQt5.QtCore import QTimer
 #                      in enumerate(c.sections) if s.data is not None]))
 
 
-@pytest.mark.skip(reason="requires actual hardware")
+@pytest.mark.require_device
 def test_main():
+    # TODO: CAMPid 03127876954165421679215396954697
+    # https://github.com/kivy/kivy/issues/4182#issuecomment-253159955
+    # fix for pyinstaller packages app to avoid ReactorAlreadyInstalledError
+    if 'twisted.internet.reactor' in sys.modules:
+        del sys.modules['twisted.internet.reactor']
+
+    import qt5reactor
     qt5reactor.install()
+
     from twisted.internet import reactor
     real_bus = can.interface.Bus(bustype='socketcan', channel='can0')
     bus = epyqlib.busproxy.BusProxy(bus=real_bus)
-    protocol = ccp.Handler()
+
+    device = epyqlib.device.Device(
+        file=epyqlib.tests.common.devices['customer'],
+        node_id=247,
+    )
+
+    tx_signal = device.neo_frames.signal_by_path(
+        'CCP', 'Connect', 'CommandCounter'
+    )
+    rx_signal = device.neo_frames.signal_by_path(
+        'CCPResponse', 'Connect', 'CommandCounter'
+    )
+    protocol = ccp.Handler(
+        endianness='little' if tx_signal.little_endian else 'big',
+        tx_id=tx_signal.frame.id,
+        rx_id=rx_signal.frame.id,
+    )
     transport = epyqlib.twisted.busproxy.BusProxy(
         protocol=protocol,
         reactor=reactor,
-        bus=bus)
+        bus=bus,
+    )
 
     d = protocol.connect()
     d.addCallback(lambda _: protocol.set_mta(
