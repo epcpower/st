0b427fad92ad36dc76a02e8797094548c804886c
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-08-17 07:57:26 -0700

    Fix paths in tests.common

--- epyqlib/tests/common.py
+++ epyqlib/tests/common.py
@@ -1,15 +1,15 @@
 import os
 
 
-interface = os.path.join('..', 'control', 'interface')
-
-devices = {
-    'customer': os.path.join(interface, 'distributed_generation.epc'),
-    'factory': os.path.join(interface, 'distributed_generation_factory.epc'),
-}
-
 project_path = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', '..', '..', '..',
 ))
 
 scripts_path = os.path.join(project_path, 'venv', 'Scripts')
+
+interface = os.path.join(project_path, '..', 'control', 'interface')
+
+devices = {
+    'customer': os.path.join(interface, 'distributed_generation.epc'),
+    'factory': os.path.join(interface, 'distributed_generation_factory.epc'),
+}
