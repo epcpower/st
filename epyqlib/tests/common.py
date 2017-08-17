1367393d914578966721c04c6ac857de9210a755
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-08-17 07:04:18 -0700

    Add project and scripts paths to tests.common
    Ref T692

--- epyqlib/tests/common.py
+++ epyqlib/tests/common.py
@@ -7,3 +7,9 @@ devices = {
     'customer': os.path.join(interface, 'distributed_generation.epc'),
     'factory': os.path.join(interface, 'distributed_generation_factory.epc'),
 }
+
+project_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', '..', '..', '..',
+))
+
+scripts_path = os.path.join(project_path, 'venv', 'Scripts')
