1057a288abab6aea8c8cc56dad0b97498e1f996c
Kyle Altendorf <sda@fstab.net>
2017-08-28 13:32:45 -0400

    Add example device files
    Ref T692

--- epyqlib/tests/common.py
+++ epyqlib/tests/common.py
@@ -1,20 +1,24 @@
 import os
 
 
+library_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', '..',
+))
+
 project_path = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', '..', '..', '..',
+    os.path.dirname(__file__), '..', '..',
 ))
 
 scripts_path = os.path.join(project_path, 'venv', 'Scripts')
 
-interface = os.path.join(project_path, '..', 'control', 'interface')
+device_path = os.path.join(library_path, 'examples', 'devices')
 
 devices = {
-    'customer': 'distributed_generation.epc',
-    'factory': 'distributed_generation_factory.epc',
+    'customer': os.path.join('customer', 'distributed_generation.epc'),
+    'factory': os.path.join('factory', 'distributed_generation_factory.epc'),
 }
 
 devices = {
-    k: os.path.normpath(os.path.join(interface, v))
+    k: os.path.normpath(os.path.join(device_path, v))
     for k, v in devices.items()
 }
