e0c34bf5673069f0158a789788fb5ccf4b53e8c3
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-09-13 23:05:29 -0400

    diagnostic prints in venv.py

--- venv.py
+++ venv.py
@@ -4,6 +4,9 @@ import os
 import sys
 import subprocess
 
+print(sys.executable)
+print(sys.version)
+
 subprocess.run([sys.executable,
                 os.path.join('sub', 'epyqlib', 'venv.py'),
                 *sys.argv[1:]])
