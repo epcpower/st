3b3797a533221585e06512085e6b9d489ddea523
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-12-20 16:29:42 -0500

    fix an import again...

--- src/epyq/__init__.py
+++ src/epyq/__init__.py
@@ -1,4 +1,4 @@
-from epyq import __version__, __sha__, __revision__
+from epyq._version import __version__, __sha__, __revision__
 
 __version_tag__ = 'v{}-{}'.format(__version__, __sha__)
 __build_tag__ = epyq._build.job_id
