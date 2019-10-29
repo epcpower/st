b604e5db9a998e9978d55bce7bd916621beb8eec
Kyle Altendorf <sda@fstab.net>
2019-10-29 12:15:08 -0400

    epyqlib for: fix comment uuid stripping when no uuid is present

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     entry_points={'gui_scripts': ['epyq = epyq.__main__:main']},
     install_requires=[
         'alqtendpy',
-        'epyqlib>=2019.9.3',
+        'epyqlib>=2019.9.10',
         'PyQt5',
         'PyQtChart',
     ],
