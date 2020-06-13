826896285191e72a1a9b6b053f97e483e449b790
Kyle Altendorf <sda@fstab.net>
2020-06-13 15:09:57 -0400

    pick the proper PCANBasic.dll per 32 vs. 64 bit

--- pinsis.py
+++ pinsis.py
@@ -44,7 +44,12 @@ installer_group.add_argument('--nsis', action='store_true')
 args = parser.parse_args()
 
 peak = 'PCANBasic.dll'
-r = requests.get('http://www.peak-system.com/produktcd//Develop/PC%20interfaces/Windows/PCAN-Basic%20API/x64/PCANBasic.dll')
+if sys.maxsize > 2**32:
+    pcanbasic_dll_url = 'http://www.peak-system.com/produktcd//Develop/PC%20interfaces/Windows/PCAN-Basic%20API/x64/PCANBasic.dll'
+else:
+    pcanbasic_dll_url = 'http://www.peak-system.com/produktcd//Develop/PC%20interfaces/Windows/PCAN-Basic%20API/Win32/PCANBasic.dll'
+
+r = requests.get(pcanbasic_dll_url)
 b = io.BytesIO(r.content)
 with open(peak, 'wb') as f:
     f.write(b.read())
