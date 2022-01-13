5110c5cc2c51bd025838a5647de703bb98a95bac
Alex Anker <alex.anker@epcpower.com>
2022-01-13 15:54:52 -0800

    black

--- buildui.py
+++ buildui.py
@@ -1,16 +1,19 @@
 import alqtendpy.compileui
 import pathlib
-#import epyqlib.buildui
+
+# import epyqlib.buildui
+
 
 def compile_ui():
     print("epyq::compile_ui building UI in epyq")
     alqtendpy.compileui.compile_ui(
-       directory_paths=[pathlib.Path(__file__).parent / "src" / "epyq"],
+        directory_paths=[pathlib.Path(__file__).parent / "src" / "epyq"],
     )
 
-
-    #todo, both of these are the same.  change it only have one
+    # todo, both of these are the same.  change it only have one
     alqtendpy.compileui.compile_ui(
-       directory_paths=[pathlib.Path(__file__).parent / "sub" / "epyqlib" / "epyqlib"],
+        directory_paths=[pathlib.Path(__file__).parent / "sub" / "epyqlib" / "epyqlib"],
     )
+
+
 #    epyqlib.buildui.compile_ui()
