1421d40192a77c6ca4f0ae6857ff635bf1fde632
Alex Anker <alex.anker@epcpower.com>
2022-01-12 18:18:58 -0800

    add epyq script, move buildui, gitignore

--- buildui.py
+++ buildui.py
@@ -1,10 +1,10 @@
 import alqtendpy.compileui
 import pathlib
+import epyqlib.buildui
 
 def compile_ui():
+    print("epyq::compile_ui building UI in epyq")
     alqtendpy.compileui.compile_ui(
        directory_paths=[pathlib.Path(__file__).parent / "src" / "epyq"],
     )
-    alqtendpy.compileui.compile_ui(
-       directory_paths=[pathlib.Path(__file__).parent / "sub" / "epyqlib" / "epyqlib"],
-    )
+    epyqlib.buildui.compile_ui()
