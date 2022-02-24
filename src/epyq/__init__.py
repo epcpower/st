3c50df63bf72039cbe8493052b7d9348dace2cb6
Gordon Schmidt <gordon.schmidt@epcpower.com>
2022-02-23 16:30:13 -0800

    feat (SC-398): cleaner versioning attempt

--- src/epyq/__init__.py
+++ src/epyq/__init__.py
@@ -1,5 +1,15 @@
-from epyq._version import __version__, __sha__, __revision__
+# For development and git commit, the __version__ is set to the build placeholder 0.0.0
+# For release, the __version__ is modified by poetry dynamic versioning with the actual released version
+__version__ = "0.0.0"
+
 import epyq._build
 
-__version_tag__ = "v{}-{}".format(__version__, __sha__)
+__version_tag__ = "v{}".format(__version__)
 __build_tag__ = epyq._build.job_id
+
+
+# from epyq._version import __version__, __sha__, __revision__
+# import epyq._build
+#
+# __version_tag__ = "v{}-{}".format(__version__, __sha__)
+# __build_tag__ = epyq._build.job_id
