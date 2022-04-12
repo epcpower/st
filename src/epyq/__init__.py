2f6909afb00051092203972b9d8a117ea75c6e68
alex anker <alex.anker@epcpower.com>
2022-04-12 15:11:00 -0700

    update comment 2

--- src/epyq/__init__.py
+++ src/epyq/__init__.py
@@ -1,5 +1,7 @@
-# For development and git commit, the __version__ is set to the build placeholder 0.0.0
-# For release, the __version__ is modified by poetry dynamic versioning with the Github tagged version
+# For development and git commit, the __version__ variable below is set
+# to the build placeholder "0.0.0". (In other words: leave alone!)
+# For release/distribution, the __version__ variable below is modified
+# during CI by poetry dynamic versioning with the github tagged version.
 __version__ = "0.0.0"
 
 import epyq._build
